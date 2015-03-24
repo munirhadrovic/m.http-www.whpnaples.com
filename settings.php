a0388543a4b3b4276944d8945d787e468800203d
novicaa <novicamiketic@gmail.com>
2015-03-24 13:15:47 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="775 First Ave., N.Naples, FL 34102"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="N.Naples, FL 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2392623399;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
